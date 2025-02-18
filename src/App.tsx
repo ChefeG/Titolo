89d601bc86dd4a505bae2c8ec9da02bc01a5cf68
chieftain <mybprislancelot@gmail.com>
2025-02-18 17:35:43 +0300

    project titolo start

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Router from 'react-router-dom'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Router>
+        
+      </Router>
+    </>
+  )
+}
+
+export default App
